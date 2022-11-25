bcaa166b4eff894d8b5d6cd8871cb74299688b19
Phạm Nhật Quan <phamnhatquan6905@gmail.com>
2022-11-25 15:04:36 +0700

    test commit 2

--- integration_test/increment_success_test.dart
+++ integration_test/increment_success_test.dart
@@ -4,16 +4,13 @@ import 'package:integration_test/integration_test.dart';
 import 'package:test_github_action/main.dart' as app;
 
 void main() {
-  final binding = IntegrationTestWidgetsFlutterBinding();
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('increment', () {
     testWidgets('success', (WidgetTester tester) async {
       app.main();
-      await binding.convertFlutterSurfaceToImage();
       await tester.pumpAndSettle();
 
-      await binding.takeScreenshot('test-screenshot');
 
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
